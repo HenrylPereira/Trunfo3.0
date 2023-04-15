2041a342f3cce8427e1a0cd142426500d2ea83d7
Henry Lopes Pereira <73833452+HenrylPereira@users.noreply.github.com>
2023-04-15 13:43:59 -0300

    Add files via upload

---
+++ src/app/mesa/mesa.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mesa',
+  templateUrl: './mesa.component.html',
+  styleUrls: ['./mesa.component.scss']
+})
+export class MesaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public quantCards = [1,2,3,4,5,6,7,8];
+
+}
